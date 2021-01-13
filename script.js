c8e5958a5ec42783fcb39f7b5ade9f786787d5d2
heartthew <onemidairmoment@gmail.com>
2021-01-13 18:05:23 -0500

    first commit fix project 4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let profileInfo = document.querySelector(".profile-info");
-let editButton = profileInfo.querySelector(".profile-info__edit-button");
+let editButton = document.querySelector(".edit-button");
 let form = document.querySelector(".form");
 let person = form.querySelector(".form__item_name");
 let job = form.querySelector(".form__item_job");
@@ -7,12 +7,23 @@ let closeIcon = form.querySelector(".form__close-icon");
 let profileInfoName = profileInfo.querySelector(".profile-info__name");
 let profileInfoOccupation = profileInfo.querySelector(".profile-info__occupation");
 
+console.log('0', `${person.value}`, `${job.value}`);
+
 person.setAttribute("value", profileInfoName.textContent);
 job.setAttribute("value", profileInfoOccupation.textContent);
 
+console.log('1', `${person.value}`, `${job.value}`);
+
 function openForm() {
+    let person = profileInfo.querySelector(".profile-info__name").textContent;
+    let job = profileInfo.querySelector(".profile-info__occupation").textContent;
+    person.setAttribute("value", profileInfoName.textContent);
+    job.setAttribute("value", profileInfoOccupation.textContent);
+    console.log('2', person, job, `${person.value}`, `${job.value}`);
+    job.textContent = `${job.value}`;
     let opened = document.querySelector(".popup");
     opened.classList.add("popup_opened");
+    console.log('3', person, job, person);
 }
 
 function closeForm() {
@@ -25,6 +36,7 @@ function handleFormSubmit(evt) {
     profileInfoName.textContent = `${person.value}`;
     profileInfoOccupation.textContent = `${job.value}`;
     closeForm();
+    console.log('4', person, job, `${person.value}`, `${job.value}`);
 }
 
 editButton.addEventListener("click", openForm);
