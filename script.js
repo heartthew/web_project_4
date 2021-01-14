a1a2e89b39b81c20b432a15eb55479543fa1f253
heartthew <onemidairmoment@gmail.com>
2021-01-14 10:04:59 -0500

    first commit fix project 4

--- script.js
+++ script.js
@@ -7,23 +7,11 @@ let closeIcon = form.querySelector(".form__close-icon");
 let profileInfoName = profileInfo.querySelector(".profile-info__name");
 let profileInfoOccupation = profileInfo.querySelector(".profile-info__occupation");
 
-console.log('0', `${person.value}`, `${job.value}`);
-
-person.setAttribute("value", profileInfoName.textContent);
-job.setAttribute("value", profileInfoOccupation.textContent);
-
-console.log('1', `${person.value}`, `${job.value}`);
-
 function openForm() {
-    let person = profileInfo.querySelector(".profile-info__name").textContent;
-    let job = profileInfo.querySelector(".profile-info__occupation").textContent;
-    person.setAttribute("value", profileInfoName.textContent);
-    job.setAttribute("value", profileInfoOccupation.textContent);
-    console.log('2', person, job, `${person.value}`, `${job.value}`);
-    job.textContent = `${job.value}`;
+    document.getElementById("name").value = profileInfoName.textContent;
+    document.getElementById("job").value = profileInfoOccupation.textContent;
     let opened = document.querySelector(".popup");
     opened.classList.add("popup_opened");
-    console.log('3', person, job, person);
 }
 
 function closeForm() {
@@ -33,10 +21,9 @@ function closeForm() {
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    profileInfoName.textContent = `${person.value}`;
-    profileInfoOccupation.textContent = `${job.value}`;
+    profileInfoName.textContent = person.value;
+    profileInfoOccupation.textContent = job.value;
     closeForm();
-    console.log('4', person, job, `${person.value}`, `${job.value}`);
 }
 
 editButton.addEventListener("click", openForm);
