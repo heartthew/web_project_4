c8718241863593c9490c7e1ac5bb2448b01bd466
heartthew <onemidairmoment@gmail.com>
2021-01-17 12:08:13 -0500

    third commit project 4

--- scripts/script.js
+++ scripts/script.js
@@ -6,17 +6,16 @@ let job = form.querySelector(".form__item_input_job");
 let closeIcon = document.querySelector(".popup__close-icon");
 let profileName = profileInfo.querySelector(".profile__name");
 let profileOccupation = profileInfo.querySelector(".profile__occupation");
+let toggle = document.querySelector(".popup");
 
 function openForm() {
     person.value = profileName.textContent;
     job.value = profileOccupation.textContent;
-    let opened = document.querySelector(".popup");
-    opened.classList.add("popup_opened");
+    toggle.classList.add("popup_opened");
 }
 
 function closeForm() {
-    let closed = document.querySelector(".popup");
-    closed.classList.remove("popup_opened");
+    toggle.classList.remove("popup_opened");
 }
 
 function handleFormSubmit(evt) {
