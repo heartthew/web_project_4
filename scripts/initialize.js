693f51f172a5d76332b571b0a8af38891d972a14
heartthew <onemidairmoment@gmail.com>
2021-02-23 15:41:45 -0500

    first checkup p7 commit

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -3,7 +3,7 @@ const starterCards = [{
         link: "https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/94779931_10217154050903814_2958008418444509184_n.jpg?_nc_cat=107&ccb=3&_nc_sid=19026a&_nc_ohc=Xy19SZ2EtuYAX9pGVrk&_nc_ht=scontent-bos3-1.xx&oh=eaeb55d0142f43f527cf9824033b779f&oe=6054DCE4"
     },
     {
-        name: "This Billboard",
+        name: "That Billboard",
         link: "https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/143754638_10219232547144921_5630130811695598075_o.jpg?_nc_cat=104&ccb=2&_nc_sid=e3f864&_nc_ohc=DAquXRQbRdUAX-R7awm&_nc_ht=scontent-bos3-1.xx&oh=0b8f417784a44f738826cc90935576fb&oe=603767FE"
     },
     {
@@ -15,7 +15,7 @@ const starterCards = [{
         link: "https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/103347923_10217531100409816_1874592485909969967_n.jpg?_nc_cat=102&ccb=3&_nc_sid=19026a&_nc_ohc=fUpp_QwZCaQAX8TDbnt&_nc_ht=scontent-bos3-1.xx&oh=155e1b2005ea40fdbe54e25fc7705543&oe=60543E8E"
     },
     {
-        name: "This Mountain",
+        name: "That Mountain",
         link: "https://scontent-bos3-1.xx.fbcdn.net/v/t1.0-9/21743115_10210336921279834_6413445036566736211_n.jpg?_nc_cat=103&ccb=3&_nc_sid=19026a&_nc_ohc=m2cZ5ciSE4cAX-alg9c&_nc_ht=scontent-bos3-1.xx&oh=9c5dce905b0ace13a79eda0f6fc07508&oe=60577D28"
     },
     {
